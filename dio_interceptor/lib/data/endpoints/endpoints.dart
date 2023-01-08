3c656257a5ee66f77ed2291dbcc884ec4151ef52
redantcode2021 <redantcyber@gmail.com>
2023-01-08 21:51:04 +0700

    Initial project

---
+++ dio_interceptor/lib/data/endpoints/endpoints.dart
@@ -0,0 +1,6 @@
+class EndPoints {
+  static const String baseUrl = "https://reqres.in";
+  static const String login = "/api/login";
+  static const String allUsers = "/api/users";
+  static const String register = "/api/register";
+}
